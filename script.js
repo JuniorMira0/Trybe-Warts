ed28ee89de1c022436449bc0da0e16ec70f383e5
JuniorMira <j-unyor14@hotmail.com>
2022-01-11 21:27:25 -0300

    requisito 18

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-let email = document.getElementById('email');
-let password = document.getElementById('password');
-let buttonEntrar = document.getElementById('buttonEntrar');
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const buttonEntrar = document.getElementById("buttonEntrar");
 
-//função para checkar email e senha
+// função para checkar email e senha
 function checkUser() {
-    if (email.value == 'tryber@teste.com' && password.value == '123456') {
-        alert('Olá, Tryber!'); 
-    } else {
-            alert('Email ou senha inválidos.');
-        }
+  if (email.value == "tryber@teste.com" && password.value == "123456") {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Email ou senha inválidos.");
+  }
 }
 
-buttonEntrar.addEventListener('click', checkUser);
+buttonEntrar.addEventListener("click", checkUser);
+
+const agreement = document.getElementById("agreement");
+const submitButton = document.getElementById("submit-btn");
+
+submitButton.disabled = true;
+
+agreement.addEventListener("click", () => {
+  if (agreement.checked) {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+});
