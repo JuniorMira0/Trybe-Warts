45bbb5ca82821948ce998756eadbd718b9a273ad
JuniorMira <j-unyor14@hotmail.com>
2022-01-14 18:24:23 -0300

    fix single quotes

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const buttonEntrar = document.getElementById("buttonEntrar");
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const buttonEntrar = document.getElementById('buttonEntrar');
 
 // função para checkar email e senha
 function checkUser() {
-  if (email.value === "tryber@teste.com" && password.value === "123456") {
-    alert("Olá, Tryber!");
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
 }
 
-buttonEntrar.addEventListener("click", checkUser);
+buttonEntrar.addEventListener('click', checkUser);
 
 // função para desabilitar botão enviar
 
-const agreement = document.getElementById("agreement");
-const submitButton = document.getElementById("submit-btn");
+const agreement = document.getElementById('agreement');
+const submitButton = document.getElementById('submit-btn');
 
 submitButton.disabled = true;
 
-agreement.addEventListener("click", () => {
+agreement.addEventListener('click', () => {
   if (agreement.checked) {
     submitButton.disabled = false;
   } else {
